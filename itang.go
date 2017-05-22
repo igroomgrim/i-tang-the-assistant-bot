1f5d925721cd779e34a0341677bd95215e52ae4d
Anak Mirasing <thaihooligan@hotmail.com>
2017-05-22 19:19:21 +0700

    Setup webhook for verification token

--- itang.go
+++ itang.go
@@ -1,20 +1,56 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 	"log"
 	"github.com/gorilla/mux"
 )
 
+const verifyToken = "itangbot"
+
 func main() {
-	router := mux.NewRouter()
-
-	router.HandleFunc("/", RootHandler)
-
-	log.Println("Start itang server at port 8888 . . .")
-	log.Fatal(http.ListenAndServe(":8888", router))
+	requestHandler()
 }
 
-func RootHandler(w http.ResponseWriter, r *http.Request) {
+func requestHandler() {
+	router := mux.NewRouter().StrictSlash(true)
+
+	router.HandleFunc("/", rootHandler).Methods("GET")
+	router.HandleFunc("/webhook", webhookHandler).Methods("GET", "POST")
+
+
+	log.Println("Start itang server at port 8000 . . .")
+	log.Fatal(http.ListenAndServe(":8000", router))
+}
+
+func rootHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("!!WOOF WOOF!!"))
+}
+
+func webhookHandler(w http.ResponseWriter, r *http.Request) {
+	
+	switch r.Method {
+		case "GET":
+			fmt.Println("webhookHandler : GET")
+			
+			query := r.URL.Query()
+
+			if query.Get("hub.verify_token") != verifyToken {
+				w.WriteHeader(http.StatusUnauthorized)
+				log.Println("Failed validation. Make sure the validation tokens match.")
+				return
+			}
+
+			w.WriteHeader(http.StatusOK)
+
+			log.Println("Validating webhook")
+
+			w.Write([]byte(query.Get("hub.challenge")))
+
+		case "POST":
+			fmt.Println("webhookHandler : POST")
+		default:
+			fmt.Println("Error request")
+	}
 }
